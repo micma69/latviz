93fa90e36bd27108b3869cf74b15ca722ecb19de
Mamdit <varokah@gmail.com>
2026-02-25 06:45:56 +0700

    add a folder named "backend" to contain backend resources from related works and start to fill the codes for lll and bkz

---
+++ app/lll/page.tsx
@@ -0,0 +1,6 @@
+import { runLLL, parseBasisFromString } from "@/backend/lll-attack-runner-main/lll-attack-runner-main/src/lib/lll"
+import Link from "next/link";
+import { Button } from "@/components/ui/button"
+import React from "react";
+
+export default function LLLPage() {}
