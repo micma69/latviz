82133abac2bd0b8fa0ecec310ab4b3ee1e5c7f00
jteo0 <jjtheodora@gmail.com>
2026-02-25 00:45:04 +0700

    sidebar idea

--- app/ml-kem/page.tsx
+++ app/ml-kem/page.tsx
@@ -18,12 +18,15 @@ export default function MLKEMPage() {
     }
 
     return (
-        <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-            <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <div className="grid grid-cols-2 flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+            <main className="flex min-h-screen w-full max-w-3xl m-4 flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+
+            </main>
+            <div className="flex min-h-screen w-full max-w-3xl m-4 flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
                 <Button variant="destructive" size="lg" onClick={() => executeMLKEM("768")}>
                     TEST ML-KEM
                 </Button>
-            </main>
+            </div>
         </div>
     );
 }
